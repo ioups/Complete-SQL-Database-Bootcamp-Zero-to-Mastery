b0a6174fecf1b6a5caa9a0172352c7eeeb4149a7
PE Daigre <pe.daigre@gmail.com>
2023-03-02 11:58:10 +0100

    exercice comparaison operators

--- SQL Deep Dive/Comparison Operators/questions.sql
+++ SQL Deep Dive/Comparison Operators/questions.sql
@@ -2,11 +2,17 @@
 /*
 * Write your query here
 */
+select count(customerid) from customers
+where gender = 'F'
+AND state = 'OR';
 
 -- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
 /*
 * Write your query here
 */
+select * from customers
+where age > 44
+AND income >= 100000;
 
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
@@ -14,8 +20,21 @@
 /*
 * Write your query here
 */
+select * from customers
+where age >= 30
+AND age <= 50
+AND income < 50000;
+
+select count(*) from customers
+where age between 30 AND 50
+  AND income < 50000;
 
 -- What is the average income between the ages of 20 and 50? (Excluding 20 and 50)
 /*
 * Write your query here
 */
+select avg(income) from customers
+where age between 21 and 49;
+
+select avg(income) from customers
+where age > 20 and age < 50;
