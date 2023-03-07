b81b929b65cd252970f88aadbf32a62dd5db9a95
PE Daigre <pe.daigre@gmail.com>
2023-03-07 16:31:37 +0100

    exercice precedence

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -7,10 +7,15 @@
 * Include people that are 50
 * that are from either Japan or Australia
 */
-
+select * from customers
+where (age < 30 or age >= 50) and income > 50000
+and (country = 'Japan' or country = 'Australia')
 /*
 * DB: Store
 * Table: Orders
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+select sum(totalalount) from orders
+where (orderdate >= '2004-06-01' and orderdate < '2004-07-01')
+and totalamount > 100;
